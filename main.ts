bcd25aa181861ee6e270890de38234e382997b3c
ManWithA1000Names <loena.phivos2010@gmail.com>
2024-11-24 16:26:43 +0200

    more written out numbers

--- main.ts
+++ main.ts
@@ -64,25 +64,68 @@ export async function* number_parts(
 
 const magnitude_label = [
   "",
-  "Thousand",
-  "Million",
-  "Billion",
-  "Trillion",
-  "Quadrillion",
-  "Quintillion",
-  "Sextillion",
-  "Septillion",
-  "Octillion",
-  "Nonillion",
-  "Decillion",
-  "Undecillion",
-  "Duodecillion",
-  "Tredecillion",
-  "Quatturodecillion",
-  "Quindecillion",
-  "Sexdecillion",
+  "thousand",
+  "million",
+  "billion",
+  "trillion",
+  "quadrillion",
+  "quintillion",
+  "sextillion",
+  "septillion",
+  "octillion",
+  "nonillion",
+  "decillion",
+  "undecillion",
+  "duodecillion",
+  "tredecillion",
+  "quatturodecillion",
+  "quindecillion",
+  "sexdecillion",
 ];
 
+const num_label = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+  "twenty",
+];
+
+const tens_label = [
+  "zeros",
+  "tens",
+  "twenty",
+  "thirty",
+  "fourty",
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety",
+];
+
+/**
+ * Removes the leading 0's, and trims the white space on the end from a string and returns it.
+ *
+ * @param {string} number The string to process.
+ * @return {string} The processed string.
+ */
 const trimLeadingZeros = (number: string): string => {
   while (number[0] === "0") {
     number = number.slice(1);
@@ -90,13 +133,59 @@ const trimLeadingZeros = (number: string): string => {
   return number.trimEnd();
 };
 
+/**
+ * Transform a string holding a valid  number below one thousand
+ * into the fully written out equivalent.
+ *
+ * If the string passed in is holding value langer that 1000, the
+ * first three characters will form the number that gets interpreted.
+ *
+ * @param {string} part The string holding the number under a thousand.
+ * @returns {string} The fully written out number
+ */
+const format_under_1000 = (part: string): string => {
+  if (part.length === 0) {
+    return "zero";
+  }
+
+  if (part.length === 1) {
+    return num_label[parseInt(part)];
+  }
+
+  if (part.length === 2) {
+    const parsed = parseInt(part);
+    if (parsed <= 20) return num_label[parsed];
+    const first = part.slice(0, 1);
+    const second = part.slice(1, 2);
+    const tens = `${tens_label[parseInt(first)]}`;
+    if (second === "0") return tens;
+    return `${tens} ${num_label[parseInt(second)]}`;
+  }
+
+  const first = part.slice(0, 1);
+  const second = part.slice(1, 2);
+  const third = part.slice(2, 3);
+  const num = `${num_label[parseInt(first)]} hunderd`;
+  if (second === "0") return num;
+  if (second === "1") return `${num} ${num_label[parseInt(part.slice(1))]}`;
+  const tens = `${tens_label[parseInt(second)]}`;
+  if (third === "0") return `${num} ${tens}`;
+  return `${num} ${tens} ${num_label[parseInt(third)]}`;
+};
+
 if (import.meta.main) {
   for await (const number of number_parts(bigint_numbers(raw_numbers()))) {
     const mapped = number
-      .map((p, i) => `${p.value} ${magnitude_label[number.length - i - 1]}`)
-      .map(trimLeadingZeros)
-      .filter((part) => !part.startsWith(" ") && part.length !== 0);
+      .map((p, i) => [
+        trimLeadingZeros(p.value),
+        magnitude_label[number.length - i - 1],
+      ])
+      .filter(([num]) => !num.startsWith(" ") && num.length !== 0);
 
-    console.log(mapped.join(", "));
+    console.log(
+      mapped
+        .map(([num, label]) => `${format_under_1000(num)} ${label}`)
+        .join(", ") || "zero",
+    );
   }
 }
